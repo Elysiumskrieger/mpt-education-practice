3f6353586e9bcead6e21c83c8b30b7b1e00d91b4
Elysiumskrieger <kurteisen@yandex.ru>
2023-10-17 23:29:28 +0300

    chore: Перемещение MvcConfig в отдельный пакет

--- src/main/java/ru/elysium/practice/config/MvcConfig.java
+++ src/main/java/ru/elysium/practice/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package ru.elysium.practice;
+package ru.elysium.practice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
